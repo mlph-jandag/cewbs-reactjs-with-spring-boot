bad6d2fcc1d23ffa8fad2142af5a8fb428d71ccc
Samuel Amador <samuel.amador@freemight.com>
2021-07-12 19:13:16 +0800

    Add route

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import "./App.css";
 import Dashboard from "./pages/dashboard/Dashboard";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+import Login from "./pages/Login/Login";
+
 
 function App() {
-  return <Dashboard />;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <Dashboard />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Redirect to="/"/>
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
