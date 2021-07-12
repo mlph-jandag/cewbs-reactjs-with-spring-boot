c91c3520a3f99c4a7c1389da696ac5f6b0b577ce
jzandag <zidrexandag10@gmail.com>
2021-07-12 17:27:51 +0800

    Added login UI

---
+++ src/components/UI/Logo/Logo.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import companyLogo from "../../../assets/images/company.png";
+import classes from "./Logo.module.css";
+
+function Logo() {
+  return (
+    <div className={classes.Logo}>
+      <img src={companyLogo} alt="MyBurger"></img>
+    </div>
+  );
+}
+
+export default Logo;
---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Logo from "../../components/UI/Logo/Logo";
+import classes from "./Login.module.css";
+import LoginForm from "./LoginForm/LoginForm";
+
+function Login() {
+  return (
+    <div className={[classes.Container, ...["d-flex"]].join(" ")}>
+      <div className={classes.LoginContainer}>
+        <div className="panel panel-default">
+          <Logo />
+          <span className={classes.title}>EMPLOYEE WELL-BEING SYSTEM</span>
+          <LoginForm />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
