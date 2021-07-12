16769cc0f65d0fb09c0e8821f6ccd0db70bf935b
Samuel Amador <samuel.amador@freemight.com>
2021-07-12 12:35:18 +0800

    add custom styles

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div className="container bg-primary">
-        container
-      </div>
+      Test
     </div>
   );
 }
