7936bbb8ed2bb8e38d60ee467acdbed8ea59a0cd
Samuel Amador <samuel.amador@freemight.com>
2021-07-13 11:33:19 +0800

    Customize sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -8,13 +8,13 @@ const Sidebar = () => {
     return (
         <div id="sidebar-container" className="sidebar-expanded d-none d-md-block">
             <ul className="list-group">
-                <a href="#submenu1" data-toggle="collapse" aria-expanded="false" className="bg-dark list-group-item list-group-item-action flex-column align-items-start">
+                <a href="#submenu1" data-toggle="collapse" aria-expanded="false" className="main-menu list-group-item list-group-item-action flex-column align-items-start">
                     <div className="d-flex w-100 justify-content-start align-items-center">
                         <span className="fa fa-dashboard fa-fw mr-3"></span>
                         <span className="menu-collapsed">Dashboard</span>
                     </div>
                 </a>
-                <a onClick={() => setShowPost(!showPost)} data-toggle="collapse" aria-expanded="false" className="bg-dark list-group-item list-group-item-action">
+                <a onClick={() => setShowPost(!showPost)} data-toggle="collapse" aria-expanded="false" className="main-menu list-group-item list-group-item-action">
                     <div className="d-flex w-100 justify-content-start align-items-center">
                         <span className="fa fa-sticky-note-o  fa-fw mr-3"></span>
                         <span className="menu-collapsed">Post</span>
@@ -22,14 +22,14 @@ const Sidebar = () => {
                     </div>
                 </a>
                 <div className={!showPost ? 'collapse' : ''}>
-                    <a href="#" className="list-group-item list-group-item-action bg-dark text-white">
+                    <a href="#" className="list-group-item list-group-item-action  ">
                         <span className="menu-collapsed">Add New</span>
                     </a>
-                    <a href="#" className="list-group-item list-group-item-action bg-dark text-white">
+                    <a href="#" className="list-group-item list-group-item-action  ">
                         <span className="menu-collapsed">View Posts</span>
                     </a>
                 </div>
-                <a  onClick={() => setShowCategory(!showCategory)} data-toggle="collapse" aria-expanded="false" className="bg-dark list-group-item list-group-item-action">
+                <a  onClick={() => setShowCategory(!showCategory)} data-toggle="collapse" aria-expanded="false" className="main-menu list-group-item list-group-item-action">
                     <div className="d-flex w-100 justify-content-start align-items-center">
                         <span className="fa fa-list-alt fa-fw mr-3"></span>
                         <span className="menu-collapsed">Category</span>
@@ -37,14 +37,14 @@ const Sidebar = () => {
                     </div>
                 </a>
                 <div className={!showCategory ? 'collapse' : ''}>
-                    <a href="#" className="list-group-item list-group-item-action bg-dark text-white">
+                    <a href="#" className="list-group-item list-group-item-action  ">
                         <span className="menu-collapsed">Add New</span>
                     </a>
-                    <a href="#" className="list-group-item list-group-item-action bg-dark text-white">
+                    <a href="#" className="list-group-item list-group-item-action  ">
                         <span className="menu-collapsed">View Categories</span>
                     </a>
                 </div>
-                <a onClick={() => setshowUser(!showUser)}  data-toggle="collapse" aria-expanded="false" className="bg-dark list-group-item list-group-item-action flex-column align-items-start">
+                <a onClick={() => setshowUser(!showUser)}  data-toggle="collapse" aria-expanded="false" className="main-menu list-group-item list-group-item-action flex-column align-items-start">
                     <div className="d-flex w-100 justify-content-start align-items-center">
                         <span className="fa fa-user fa-fw mr-3"></span>
                         <span className="menu-collapsed">Users</span>
@@ -52,10 +52,10 @@ const Sidebar = () => {
                     </div>
                 </a>
                 <div className={!showUser ? 'collapse': ''}>
-                    <a href="#" className="list-group-item list-group-item-action bg-dark text-white">
+                    <a href="#" className="list-group-item list-group-item-action  ">
                         <span className="menu-collapsed">Add New</span>
                     </a>
-                    <a href="#" className="list-group-item list-group-item-action bg-dark text-white">
+                    <a href="#" className="list-group-item list-group-item-action  ">
                         <span className="menu-collapsed">View Users</span>
                     </a>
                 </div>
