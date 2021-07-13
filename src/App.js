113d20716008c18b01b190a1c122756102b4eac0
Rovin Leano <rovin.leano@monstar-lab.com>
2021-07-13 18:23:53 +0800

    Added UI for creating new post

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import Dashboard from "./pages/dashboard/Dashboard";
 import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 import Login from "./pages/Login/Login";
-
+import CreatePost from "./pages/posts/CreatePost";
 
 function App() {
   return (
@@ -13,6 +13,9 @@ function App() {
         </Route>
         <Route path="/login">
           <Login />
+        </Route>
+        <Route path="/create-post">
+          <CreatePost />
         </Route>
         <Redirect to="/"/>
       </Switch>
