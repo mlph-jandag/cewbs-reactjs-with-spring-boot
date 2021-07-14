57b1e160353f0a44a61202ea1393bdb835836cd4
jzandag <john.andag@monstar-lab.com>
2021-07-14 12:46:17 +0800

    Added saving of post to firebase

--- src/components/Editor/Editor.jsx
+++ src/components/Editor/Editor.jsx
@@ -1,23 +1,35 @@
 import React, { useEffect, useState } from "react";
 import { Editor } from "react-draft-wysiwyg";
-import { EditorState } from "draft-js";
+import { EditorState, convertToRaw } from "draft-js";
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 
-export default function TextEditor() {
-    const [editorState, setEditorState] = useState(() =>
-        EditorState.createEmpty()
-    );
-    useEffect(() => {
-        console.log(editorState);
-    }, [editorState]);
-    return (
-        <div>
-            <div style={{ border: "1px solid black", padding: '2px', minHeight: '400px' }}>
-                <Editor
-                    editorState={editorState}
-                    onEditorStateChange={setEditorState}
-                />
-            </div>
-        </div>
-    );
+export default function TextEditor({ onChangedHandler }) {
+  const [editorState, setEditorState] = useState(() =>
+    EditorState.createEmpty()
+  );
+  useEffect(() => {
+    console.log(convertToRaw(editorState.getCurrentContent()));
+  }, [editorState]);
+
+  const onChangeHandler = (e) => {
+    setEditorState(e);
+    onChangedHandler(convertToRaw(editorState.getCurrentContent()));
+  };
+
+  return (
+    <div>
+      <div
+        style={{
+          border: "1px solid black",
+          padding: "2px",
+          minHeight: "400px",
+        }}
+      >
+        <Editor
+          editorState={editorState}
+          onEditorStateChange={onChangeHandler}
+        />
+      </div>
+    </div>
+  );
 }
--- src/pages/posts/CreatePost.jsx
+++ src/pages/posts/CreatePost.jsx
@@ -1,46 +1,86 @@
-import React, { useState } from 'react';
-import Navbar from '../../components/Navbar/Navbar';
-import Sidebar from '../../components/Sidebar';
-import TextEditor from '../../components/Editor/Editor';
+import React, { useState } from "react";
+import Navbar from "../../components/Navbar/Navbar";
+import Sidebar from "../../components/Sidebar";
+import TextEditor from "../../components/Editor/Editor";
 import classes from "./CreatePost.module.css";
-import CategoryDropDown from '../../components/Buttons/CategoryDropDown';
+import CategoryDropDown from "../../components/Buttons/CategoryDropDown";
+import { EditorState } from "draft-js";
+import { firestore } from "../../firebase.config";
 
 const CreatePost = () => {
-    const [title, setTitle] = useState("");
-    return (
-        <>
-            <Navbar />
-            <div className="row mr-0 ml-0">
-                <Sidebar />
-                <div className="col p-4">
-                    <div className="card mt-2">
-                        <h5 className="card-header font-weight-heavy">New Post</h5>
-                        <div className="card-body">
-                            <div class="form-group">
-                                <input
-                                    className={classes.input}
-                                    type="text"
-                                    class="form-control"
-                                    id="title"
-                                    name="title"
-                                    value={title}
-                                    placeholder="Enter post title"
-                                    onChange={(e) => setTitle(e.target.value)}
-                                />
-                            </div>
-                            <TextEditor></TextEditor>
-                            <div className={classes.bottomActions}>
-                                <div className={classes.drop}>
-                                    <CategoryDropDown></CategoryDropDown>
-                                </div>
-                                <button className={classes.postButton}>Post</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </>
-    )
-}
+  const [title, setTitle] = useState("");
+  const [category, setCategory] = useState("");
+  const [editorState, setEditorState] = useState(() =>
+    EditorState.createEmpty()
+  );
 
-export default CreatePost
+  const onSaveHandler = async () => {
+    //TODO
+    console.log("body", editorState);
+    console.log("title", title);
+    console.log("category", category);
+    console.log("created_at", new Date().toDateString());
+    try {
+      const response = firestore.collection("posts");
+      const id = await response.doc().id;
+      await response.doc(id).set({
+        body: editorState,
+        title: title,
+        category: category,
+        created_at: new Date().toDateString(),
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const onCategoryChanged = (data) => {
+    console.log(data);
+    setCategory(data);
+  };
+
+  const getEditorState = (data) => {
+    setEditorState(data);
+  };
+
+  return (
+    <>
+      <Navbar />
+      <div className="row mr-0 ml-0">
+        <Sidebar />
+        <div className="col p-4">
+          <div className="card mt-2">
+            <h5 className="card-header font-weight-heavy">New Post</h5>
+            <div className="card-body">
+              <div className="form-group">
+                <input
+                  className={[classes.input, "form-control"].join(" ")}
+                  type="text"
+                  id="title"
+                  name="title"
+                  value={title}
+                  placeholder="Enter post title"
+                  onChange={(e) => setTitle(e.target.value)}
+                />
+              </div>
+              <TextEditor onChangedHandler={getEditorState} />
+              <div className={classes.bottomActions}>
+                <div className={classes.drop}>
+                  <CategoryDropDown
+                    category={category}
+                    onCategoryChanged={onCategoryChanged}
+                  />
+                </div>
+                <button onClick={onSaveHandler} className={classes.postButton}>
+                  Post
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default CreatePost;
