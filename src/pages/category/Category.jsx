5c1c4fade28e3d7a1765b0440f48eda90daebbc9
Samuel Amador <samuel.amador@freemight.com>
2021-07-15 16:15:00 +0800

    Category form

--- src/pages/category/Category.jsx
+++ src/pages/category/Category.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import DefaultLayout from '../../components/Layouts/DefaultLayout';
+import CategoryList from './CategoryList';
+import CategoryForm from './CategoryForm/CategoryForm';
 
 const Category = () => {
     return (
         <DefaultLayout>
-            Test category
+            <h3 className="mb-4">
+                <span className="fa fa-list-alt fa-fw"></span> Category
+            </h3>
+            <div className="row">
+                <div className="col-5">
+                    <CategoryForm />
+                </div>
+                <div className="col">
+                    <CategoryList />
+                </div>
+            </div>
         </DefaultLayout>
     )
 }
---
+++ src/pages/category/CategoryList.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CategoryList = () => {
+    return (
+        <div>
+            list
+        </div>
+    )
+}
+
+export default CategoryList
