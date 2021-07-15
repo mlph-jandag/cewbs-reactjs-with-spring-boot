5c6db2092dcdc11240870804f460f5b3ca00f97a
Samuel Amador <samuel.amador@freemight.com>
2021-07-15 16:54:25 +0800

    On delete

--- src/pages/category/CategoryActions.jsx
+++ src/pages/category/CategoryActions.jsx
@@ -1,13 +1,23 @@
-import React from 'react'
+import React from 'react';
+import { firestore } from '../../firebase.config';
 
 const CategoryActions = ({ data }) => {
-  console.log(data);
+
+  const onDeleteHandler =  async () => {
+    try {
+      await firestore.collection("categories").doc(data.uid).delete();
+    } catch (e) {
+      console.log(e);
+    }
+  } 
   return (
-    <div className="d-flex justify-content-around">
+    <div className="d-flex justify-content-around actions">
       <span>
         <i className="fa fa-pencil text-info"></i>
       </span>
-      <span>
+      <span
+        onClick={onDeleteHandler}
+      >
         <i className="fa fa-trash-o text-danger"></i>
       </span>
     </div>
