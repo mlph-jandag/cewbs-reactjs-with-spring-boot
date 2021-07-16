8bb3de937ff6b888ceab41ac9e6d3e89dc4ee9d9
jzandag <john.andag@monstar-lab.com>
2021-07-16 10:47:14 +0800

    added filter on viewpost

--- src/components/Buttons/CategoryButtons.jsx
+++ src/components/Buttons/CategoryButtons.jsx
@@ -1,8 +1,12 @@
 import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
 import { firestore } from '../../firebase.config';
+import { setCategory } from '../../slices/postSlice';
 
 const CategoryButtons = () => {
-    const [cats, setCats] = useState([]);
+    const [cats, setCats] = useState(['all']);
+    const dispatch = useDispatch();
+    const categoryState = useSelector(state => state.post.category);
 
     const fetchCategories = async () => {
         try {
@@ -21,14 +25,16 @@ const CategoryButtons = () => {
         fetchCategories();
     }, []);
 
+    const onClickCategoryHandler = (cat) => {
+        dispatch(setCategory(cat));
+      };
 
     return (
         <div className="category-buttons">
-            <a href="/" className="btn-yellow toupper">All </a>
             {
                 cats.map((cat, index) => {
                     return (
-                        <a href="/" className="btn-yellow toupper" key={index}>
+                        <a href="#" onClick={() => onClickCategoryHandler(cat)} className="btn-yellow toupper" key={index}>
                             { cat }
                         </a>
                     );
