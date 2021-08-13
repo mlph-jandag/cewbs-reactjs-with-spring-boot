f64aa8a9471845bb8ed93a7f8ee32d081d04f87b
Samuel Amador <samuel.amador@freemight.com>
2021-08-13 20:50:52 +0800

    Add authslice

--- src/slices/store.js
+++ src/slices/store.js
@@ -2,11 +2,13 @@ import {configureStore} from '@reduxjs/toolkit';
 import companySlice from './companySlice';
 import postSlice from './postSlice';
 import serviceSlice from './serviceSlice';
+import authSlice from './authSlice';
 
 export default configureStore({
   reducer: {
     post: postSlice,
     service: serviceSlice,
-    company: companySlice
+    company: companySlice,
+    auth: authSlice,
   },
 });
