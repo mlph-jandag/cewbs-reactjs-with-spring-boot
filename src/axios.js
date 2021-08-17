5120df86c3f856314777612bcb332a8fe8a2419c
Rovin Leano <rovin.leano@monstar-lab.com>
2021-08-17 12:32:51 +0800

    Added pagination in categories

--- src/axios.js
+++ src/axios.js
@@ -6,6 +6,7 @@ const instance = axios.create({
   baseURL: API_URL, 
   headers: authHeader()
 });
+
 instance.defaults.headers.common["Access-Control-Allow-Origin"] = "*";
 instance.defaults.headers.common["Access-Control-Allow-Methods"] = "GET,PUT,POST,DELETE,PATCH,OPTIONS";
 
