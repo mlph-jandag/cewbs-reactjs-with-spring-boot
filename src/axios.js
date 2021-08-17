616188aec25765d38a9e85ee44daa8f5abe0586e
mlph-jandag <john.andag@monstar-lab.com>
2021-08-17 08:44:30 +0800

    refactore codes

--- src/axios.js
+++ src/axios.js
@@ -1,10 +1,9 @@
-import axios from 'axios'
+import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'http://localhost:8080/'
-})
-instance.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
-instance.defaults.headers.common['Access-Control-Allow-Methods'] = 'GET,PUT,POST,DELETE,PATCH,OPTIONS';
+  baseURL: "http://localhost:8080/api",
+});
+instance.defaults.headers.common["Access-Control-Allow-Origin"] = "*";
+instance.defaults.headers.common["Access-Control-Allow-Methods"] = "GET,PUT,POST,DELETE,PATCH,OPTIONS";
 
-
-export default instance
+export default instance;
--- src/routes/AdminRoutes.js
+++ src/routes/AdminRoutes.js
@@ -1,15 +1,13 @@
 import React from "react";
 import Dashboard from "../pages/dashboard/Dashboard";
 import Category from "../pages/category/Category";
-import UserList from "../pages/users/UserList";
 import CreatePost from "../pages/posts/CreatePost/CreatePost";
 import AuthenticatedRoute from "./AuthenticatedRoute";
 import CreatePartner from "../pages/partners/CreatePartner/CreatePartner";
-import ViewPartners from "../pages/partners/ViewPartners/ViewPartners.jsx";
 import ViewPosts from "../pages/posts/ViewPost/ViewPost";
 import ViewServices from "../pages/services/ViewServices/ViewServices";
 import PostCategory from "../pages/posts/PostCategory/PostCategory";
-import ViewUsers from '../pages/users/ViewUsers/ViewUsers'
+import ViewUsers from "../pages/users/ViewUsers/ViewUsers";
 import Partner from "../pages/partners/Partner";
 
 const AdminRoutes = () => {
@@ -18,11 +16,11 @@ const AdminRoutes = () => {
       <AuthenticatedRoute exact path="/" component={Dashboard} />
       <AuthenticatedRoute path="/categories" component={Category} />
       <AuthenticatedRoute exact path="/create-post" component={CreatePost} />
-      <AuthenticatedRoute exact path="/posts/category/:cat" component={PostCategory}/>
+      <AuthenticatedRoute exact path="/posts/category/:cat" component={PostCategory} />
       <AuthenticatedRoute path="/posts" component={ViewPosts} />
       <AuthenticatedRoute path="/partners" component={Partner} />
-      <AuthenticatedRoute path="/create-partner/:uid" component={CreatePartner}/>
-      <AuthenticatedRoute exact path="/create-partner" component={CreatePartner}/>
+      <AuthenticatedRoute path="/create-partner/:uid" component={CreatePartner} />
+      <AuthenticatedRoute exact path="/create-partner" component={CreatePartner} />
       <AuthenticatedRoute path="/services/:uid" component={ViewServices} />
       <AuthenticatedRoute path="/create-post/:uid" component={CreatePost} />
       <AuthenticatedRoute path="/users" component={ViewUsers} />
